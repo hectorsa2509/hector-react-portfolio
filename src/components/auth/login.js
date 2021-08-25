8aa181a2a22953034d8a4fa3c892d45cc27f45e7
Hector Santaella <santaella@ciencias.unam.mx>
2021-08-24 22:37:00 -0500

    Handling Sign In Error in React # 2397

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,3 +1,4 @@
+
 import React, { Component } from "react";
 import axios from "axios";
 
@@ -7,7 +8,8 @@ export default class Login extends Component {
 
     this.state = {
       email: "",
-      password: ""
+      password: "",
+      errorText: ""
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -16,7 +18,8 @@ export default class Login extends Component {
 
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
+      errorText: ""
     });
   }
 
@@ -33,7 +36,18 @@ export default class Login extends Component {
         { withCredentials: true }
       )
       .then(response => {
-        console.log("response", response);
+        if (response.data.status === "created") {
+          console.log("You can come in...");
+        } else {
+          this.setState({
+            errorText: "Wrong email or password"
+          });
+        }
+      })
+      .catch(error => {
+        this.setState({
+          errorText: "An error occurred"
+        });
       });
 
     event.preventDefault();
@@ -43,6 +57,8 @@ export default class Login extends Component {
     return (
       <div>
         <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+        <div>{this.state.errorText}</div>
 
         <form onSubmit={this.handleSubmit}>
           <input
